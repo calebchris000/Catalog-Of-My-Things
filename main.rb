92e920118b439feac437a38ad43b9952a1e8c6dd
Musong <ernestakeh@gmail.com>
2023-05-29 16:49:07 +0100

    Add date argurment to constructor in main.rb

--- main.rb
+++ main.rb
@@ -5,16 +5,14 @@ class Main
   include Options
 
   def initialize
-    @item = Item.new
+     @item = Item.new('2023-01-12')
   end
 
     puts "Welcome to Catalog of my things\n\n"
     puts "Please choose an option by entering a number!\n"
 
   def run
-    loop do
       options
-    end
   end
 end
 
