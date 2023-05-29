1846c92e8cb726585c725e2d99282409a3849003
shubham <shubham.bhoyar077@gmail.com>
2023-05-29 21:08:59 +0530

    Fix linters error

--- music_album/music_album_store.rb
+++ music_album/music_album_store.rb
@@ -1,8 +1,9 @@
 require 'json'
 
 class MusicAlbumStore
+  attr_reader :music_album_file
   def initialize
-    @music_album_file = "music_album/music_album.json"
+    @music_album_file = 'music_album/music_album.json'
   end
 
   def file_read(filename)
@@ -11,17 +12,14 @@ class MusicAlbumStore
   end
 
   def file_write(filename, data)
-    puts data
     File.write(filename, JSON.generate(data))
   end
 
   def store_music_album(music_album)
-    #music_album = {publish_date, on_spotify, genre}
+    # music_album = {publish_date, on_spotify, genre}
     music_albums = []
     music_albums = file_read(@music_album_file) if File.exist?(@music_album_file)
     music_albums << music_album
     file_write(@music_album_file, music_albums)
   end
-
- 
 end
