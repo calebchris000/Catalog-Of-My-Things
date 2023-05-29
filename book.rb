46d3ba30e344382606e147a8b0f7d681a99191eb
Caleb Nwaizu <calebchris000@gmail.com>
2023-05-29 16:24:13 +0100

    Book class

---
+++ book.rb
@@ -0,0 +1,17 @@
+require './item'
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    puts super
+    super || @cover_state == 'bad' ? true : false
+  end
+end
