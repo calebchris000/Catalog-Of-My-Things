6c5612c91fc07512dfd30fbdd17eb63560654d61
Musong <ernestakeh@gmail.com>
2023-05-29 16:13:59 +0100

    Add options

--- main.rb
+++ main.rb
@@ -1,1 +1,22 @@
-# main.rb
+require_relative './classes/item'
+require_relative './modules/options'
+ 
+class Main
+  include Options
+
+  def initialize
+    @item = Item.new
+  end
+
+    puts "Welcome to Catalog of my things\n\n"
+    puts "Please choose an option by entering a number!\n"
+
+  def run
+    loop do
+      options
+    end
+  end
+end
+
+main = Main.new
+main.run
