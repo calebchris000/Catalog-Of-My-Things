f813329067ce62246399a4949a176278d3b33f73
Musong <ernestakeh@gmail.com>
2023-05-30 12:25:09 +0100

    Add test for game and author classes

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,30 +1,14 @@
-require './game/game'
+require './game/author'
 
-describe Student do
-    context '#play_hooky' do
-      it 'should return a string with the shrug emoji' do
-        student = Student.new(name: 'Musong', age: 21, parent_permission: false, classroom: 'Unknown')
-        expect(student.play_hooky).to eq('¯\\_(ツ)_/¯')
-      end
+describe Author do
+    context '#add_item' do
+      it 'returns correct First name' do
+        author = Author.new('Musong', 'Akeh')
+        expect(author.first_name).to eq('Musong')
+      end  
+      it 'returns correct Last name' do
+        author = Author.new('Musong', 'Akeh')
+        expect(author.last_name).to eq('Akeh')
+      end  
     end
-  
-    context '#add_classroom' do
-      it 'should add the student to the given classroom' do
-        student = Student.new(name: 'Musong', age: 21, parent_permission: false, classroom: 'Unknown')
-        classroom = Classroom.new('Class one')
-  
-        student.add_classroom(classroom)
-  
-        expect(classroom.students).to include(student)
-      end
-  
-      it 'should not add the student to the classroom if the student is already in the classroom' do
-        student = Student.new(name: 'Musong', age: 21, parent_permission: false, classroom: 'Unknown')
-        classroom = Classroom.new('Class one')
-  
-        student.add_classroom(classroom)
-  
-        expect { student.add_classroom(classroom) }.not_to change { classroom.students.count }
-      end
-    end
-  end
+end
