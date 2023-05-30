0dfb2df381073c33855aa2dc9908953809bf4987
shubham <shubham.bhoyar077@gmail.com>
2023-05-30 14:45:45 +0530

    Create SQL query for music and genre

--- schema.sql
+++ schema.sql
@@ -1,1 +1,16 @@
 -- Add your create table query as per instruction.
+
+CREATE TABLE genre (
+  id INT,
+  name varchar(100),
+  PRIMARY KEY (id),
+);
+
+CREATE TABLE musicalbum (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE,
+    on_spotify BOOLEAN,
+    genre_id INT,
+    PRIMARY KEY(id)
+    CONSTRAINT fk_genre FOREIGN KEY(genre_id) REFERENCES genre(id),
+);
